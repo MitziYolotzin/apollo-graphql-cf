c5cfde027dcfba588f2ea4d2dd6169b4c28af4c2
MitziYolotzin <mitziulter@gmail.com>
2020-04-26 18:09:01 -0500

    mutation for add new course

--- server.js
+++ server.js
@@ -11,29 +11,49 @@ const typeDefs = `
         views: Int
     }
 
+    input CourseInput {
+        title: String!
+        views: Int
+    }
+
     type Query {
         getCourses(page: Int, limit: Int = 1): [Course]
     }
 
+    type Mutation {
+        addCourse(input: CourseInput): Course
+    }
+
 `;
 
-const schema = makeExecutableSchema({
-    typeDefs: typeDefs,
-    resolvers: {
-        Query: {
-            getCourses(obj, { page, limit }) {
-                if (page !== undefined) {
-                    return courses.slice(page * limit, (page + 1) * limit);
-                    //init in 0
-                    //return courses.slice((page-1) * limit, (page) * limit);
-                }
-                return courses;
+const resolvers = {
 
+    Query: {
+        getCourses(obj, { page, limit }) {
+            if (page !== undefined) {
+                return courses.slice(page * limit, (page + 1) * limit);
+                //init in 0
+                //return courses.slice((page-1) * limit, (page) * limit);
             }
+            return courses;
+        }
+    },
+    Mutation: {
+        addCourse(obj, { input }) {
+            const id = String(courses.length + 1);
+            const course = { id, ...input }; //spread operator, extract properties and combine with first obj
+            courses.push(course);
+            return course;
         }
     }
 
-})
+}
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+
+});
 
 const server = new ApolloServer({
     schema: schema
